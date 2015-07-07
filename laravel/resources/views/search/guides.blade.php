99be1cf5381294a88336e12f8f8fa764afe463d7
krussll <raw987@hotmail.com>
2015-07-07 23:02:25 +0100

    template changes

--- laravel/resources/views/search/guides.blade.php
+++ laravel/resources/views/search/guides.blade.php
@@ -2,30 +2,34 @@
 
 @section('content')
         
-     <div ng-controller="searchController" ng-init="search.init('[[$location]]')" class="row" ng-cloak>
+     <div ng-controller="searchController" ng-init="search.init('[[$location]]')" ng-cloak>
       <!-- page wrap -->
-      <div class="twelve columns">
-      <h1>Guides - [[$location]]</h1> 
-        <img src='/images/ajax-loader.gif' ng-show="search.control.isLoading" />
-        <div id="container" ng-hide="search.control.isLoading" >
-            
-          <!-- photo -->
-          <div class='box_fluid col_fluid' ng-repeat="guide in search.profiles">
-              <a href="#"><img src='/images/bath.jpeg' alt="desc" /></a>
-                    <div class='box_fluid_inner'>
-                        <a href="#" class="media-photo-badge">
-                          <div class="media-photo media-round">
-                              <img src="/images/profile.jpg" />
-                          </div>
-                        </a>
-                        <h4><strong>{{guide.firstname}} {{guide.surname}}</strong></h4>
-                        <div class="descrp">
-                          <div>One chilled out Mother</div>
-                          <div>4.5 stars · 7 reviews</div>
-                        <div>
-                    </div>
+      <section class="section_light">
+        <div  class="row">
+          <div class="twelve columns">
+          <h1>Guides - [[$location]]</h1> 
+            <img src='/images/ajax-loader.gif' ng-show="search.control.isLoading" />
+            <div id="container" ng-hide="search.control.isLoading" >
+                
+              <!-- guide -->
+              <div class='box_fluid col_fluid' ng-repeat="guide in search.profiles">
+                  <a href="#"><img src='/images/bath.jpeg' alt="desc" /></a>
+                        <div class='box_fluid_inner'>
+                            <a href="#" class="media-photo-badge">
+                              <div class="media-photo media-round">
+                                  <img src="/images/profile.jpg" />
+                              </div>
+                            </a>
+                            <h4><strong>{{guide.firstname}} {{guide.surname}}</strong></h4>
+                            <div class="descrp">
+                              <div>One chilled out Mother</div>
+                              <div>4.5 stars · 7 reviews</div>
+                            <div>
+                        </div>
+              </div>
+            </div>
           </div>
         </div>
-        </div> 
+      </section>
   </div>
 @stop
